6c705b8500909139d6ec8cd4714bd6d75aa72614
David Alvarez S <davalvsil@alum.us.es>
2015-01-18 18:34:41 +0100

    Creada clase.java

---
+++ README.md
@@ -0,0 +1,1 @@
+En este repositorio se harán pruebas de cara al examen para subir nota de GIT, asignatura EGC.
---
+++ clase.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class clase{
+	private String cadena;
+	
+	public clase(){
+	}
+}
