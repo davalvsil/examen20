2aaf683c6c94e3fbc49c5014f79a2beee1e6c24f
David Alvarez S <davalvsil@alum.us.es>
2015-01-18 20:51:10 +0100

    Cambios de definitivos, version estable

--- clase.java
+++ clase.java
@@ -4,15 +4,24 @@ public class clase{
 	private String cadena;
 	private Integer numerito;
 	
+	/*
+	*Constructor
+	*/	
 	public clase(String in_cadena){
 		this.cadena = in_cadena;
 		this.numerito = 69;
 	}
 	
+	/*
+	*GetCadena
+	*/
 	public String getCadena(){
 		return this.cadena;
 	}
 	
+	/*
+	*GetNumerito
+	*/
 	public Integer getNumerito(){
 		return this.numerito;
 	}
