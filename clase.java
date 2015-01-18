97b7a332814eca0e3c0c1f76aeb84d06d6e45b7b
David Alvarez S <davalvsil@alum.us.es>
2015-01-18 20:04:20 +0100

    Insertada la variable numeraco

--- clase.java
+++ clase.java
@@ -2,9 +2,11 @@ import java.util.*;
 
 public class clase{
 	private Integer numerito;
+	private Long numeraco;
 	
 	public clase(){
 		this.numerito = 69;
+		this.numeraco = 6969;
 	}
 	
 	public Integer getNumerito(){
