99b3268b335a30e4b5016dfb6f43ea8548d5e43a
David Alvarez S <davalvsil@alum.us.es>
2015-01-18 20:38:52 +0100

    Revertidos los cambios y subiendo

--- clase.java
+++ clase.java
@@ -2,14 +2,12 @@ import java.util.*;
 
 public class clase{
 	private Integer numerito;
-	private Long numeraco;
 	
 	public clase(){
 		this.numerito = 69;
-		this.numeraco = 6969;
 	}
 	
 	public Integer getNumerito(){
-		return.this numerito;
+		return this.numerito;
 	}
 }
